71a3fbfe7af695a57e537bcc8a48ea5cd1a7dc3c
GuilhermeQuirinoCruz <guilhermequirinocruz@gmail.com>
2023-05-09 22:36:48 -0300

    tipo string adicionado

---
+++ tipos/string.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+typedef struct
+{
+    char *valor;
+} String;
+
+String *nova_string(char *valor)
+{
+    String *string = (String *) malloc(sizeof(String));
+    string->valor = strdup(valor);
+
+    return string;
+}
+
+void liberar_string(void *string)
+{
+    String *string_liberar = (String *)string;
+    free(string_liberar->valor);
+    free(string_liberar);
+}
+
+int comparar_strings(void *string1, void *string2)
+{
+    String *str1 = (String *)string1;
+    String *str2 = (String *)string2;
+
+    return strcmp(str1->valor, str2->valor);
+}
+
+void alterar_string(void *string, void *dados)
+{
+    String *str = (String *)string;
+
+    free(str->valor);
+    str->valor = strdup((char *)dados);
+}
+
+void imprimir_string(void *string)
+{
+    String *str = (String *)string;
+    printf("%s", str->valor);
+}
